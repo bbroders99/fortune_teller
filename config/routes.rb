b27d3f6910a5210998af2d7a3c20e6a8dc1e1c50
Ben Morales <b-morales@kellogg.northwestern.edu>
2013-10-20 19:23:05 -0500

    Rails server setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 FortuneTeller::Application.routes.draw do
+
+  get("/lucky_numbers", { :controller => "fortunes", :action => "numbers" })
+
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
